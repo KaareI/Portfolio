2d4b6057b8dcb10651bd9f1dd5a0bda0cd9d9f02
KaareI <114857354+KaareI@users.noreply.github.com>
2023-08-17 21:01:53 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,7 @@ function fadeIn(el, display) {
     el.style.opacity = 0;
     el.style.display = display || "block";
     (function fade() {
-        const val = parseFloat(el.style.opacity);
+        let val = parseFloat(el.style.opacity);
         if (!((val += .1) > 1)) {
             el.style.opacity = val;
             requestAnimationFrame(fade);
